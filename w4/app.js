049ea26ac81f36ca1a1fea594a226940342774a6
erichight <erichight81@yahoo.com>
2022-09-02 04:29:41 +0000

    w4

--- w4/app.js
+++ w4/app.js
@@ -5,7 +5,6 @@ const showOnPage = function (text) {
   outputDiv.append(newParagraph);
 };
 
-
 const gameDecision = {
   freeTime: 0,
   dayOfWeek: '',
@@ -13,31 +12,31 @@ const gameDecision = {
   kidsBedtime: 12,
   isWeekend: true,
   homeworkDone: true,
- 
-setWeekend: function(value){
-    this.isWeekend = value
-},
-setHomeworkDone: function(value) {
-    this.homeworkDone = value
- },
-setDay: function(day) {
-   this.dayOfWeek = day
-},
-setTime: function (time){
-  this.currentTime = time
-},
-determineKidsBedtime: function(day) {
-  this.dayOfWeek = day 
-  if (day === 'Friday' || 'Saturday'){
-   // this.isWeekend = true;
-    this.kidsBedtime = 12;
-  //  return this.kidsBedtime;
-  }
-  else (day === 'Monday'||'Tuesday'||'Wednesday'||'Thursday'||'Sunday')
-  //  this.isWeekend = false; 
+
+  setWeekend: function (value) {
+    this.isWeekend = value;
+  },
+  setHomeworkDone: function (value) {
+    this.homeworkDone = value;
+  },
+  setDay: function (day) {
+    this.dayOfWeek = day;
+  },
+  setTime: function (time) {
+    this.currentTime = time;
+  },
+  determineKidsBedtime: function (day) {
+    this.dayOfWeek = day;
+    if (day === 'Friday' || 'Saturday') {
+      // this.isWeekend = true;
+      this.kidsBedtime = 12;
+      //  return this.kidsBedtime;
+    } else
+      day === 'Monday' || 'Tuesday' || 'Wednesday' || 'Thursday' || 'Sunday';
+    //  this.isWeekend = false;
     this.kidsBedtime = 9;
 
-  //  return this.kidsBedtime;
+    //  return this.kidsBedtime;
   },
   displayOut: function (time, bedtime, homework, weekend) {
     let freeTime = this.kidsBedtime - this.currentTime;
@@ -52,12 +51,18 @@ determineKidsBedtime: function(day) {
     showOnPage('<b><:::::::::::::::::::::::::::::::::::></b>');
     if (this.isWeekend && this.homeworkDone) {
       showOnPage('Order some pizza and game on!!');
-    } else if (this.isWeekend === false && this.homeworkDone === true && freeTime >= 0.51) {
+    } else if (
+      this.isWeekend === false &&
+      this.homeworkDone === true &&
+      freeTime >= 0.51
+    ) {
       showOnPage(`Yes, you can play for ${freeTime} hours`);
-    } else if (this.isWeekend === false && this.homeworkDone === true && freeTime < 0.51) {
-      showOnPage(
-        `There is only ${freeTime} hours before the kids go to bed`
-      );
+    } else if (
+      this.isWeekend === false &&
+      this.homeworkDone === true &&
+      freeTime < 0.51
+    ) {
+      showOnPage(`There is only ${freeTime} hours before the kids go to bed`);
       showOnPage(
         'That is not enought time for video games. Have the kids brush their teeth, and read them a story instead'
       );
@@ -67,15 +72,31 @@ determineKidsBedtime: function(day) {
       showOnPage('The kids need to do thier homework before playing');
     } else {
       showOnPage('No, it is a school night and the kids have homework!');
-      showOnPage(`They need to finish before thier bedtime at ${this.kidsBedtime}`);
+      showOnPage(
+        `They need to finish before thier bedtime at ${this.kidsBedtime}`
+      );
     }
     showOnPage('<b><:::::::::::::::::::::::::::::::::::></b>');
-  }
-}
-gameDecision.displayOut(gameDecision.setTime(7),gameDecision.determineKidsBedtime('Monday'),gameDecision.setHomeworkDone(false),gameDecision.setWeekend(false));
-gameDecision.displayOut(gameDecision.setTime(8.5),gameDecision.determineKidsBedtime('Tuesday'),gameDecision.setHomeworkDone(true),gameDecision.setWeekend(false));
-gameDecision.displayOut(gameDecision.setTime(6),gameDecision.determineKidsBedtime('Saturday'),gameDecision.setHomeworkDone(true),gameDecision.setWeekend(true));
-
+  },
+};
+gameDecision.displayOut(
+  gameDecision.setTime(7),
+  gameDecision.determineKidsBedtime('Monday'),
+  gameDecision.setHomeworkDone(false),
+  gameDecision.setWeekend(false)
+);
+gameDecision.displayOut(
+  gameDecision.setTime(8.5),
+  gameDecision.determineKidsBedtime('Tuesday'),
+  gameDecision.setHomeworkDone(true),
+  gameDecision.setWeekend(false)
+);
+gameDecision.displayOut(
+  gameDecision.setTime(6),
+  gameDecision.determineKidsBedtime('Saturday'),
+  gameDecision.setHomeworkDone(true),
+  gameDecision.setWeekend(true)
+);
 
 //gameDecision.setDay('Monday')
 //showOnPage(gameDecision.dayOfWeek)
